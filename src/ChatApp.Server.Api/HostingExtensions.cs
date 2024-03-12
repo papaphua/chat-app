e09488d3106a8d367b590afa241f104e1d2a96c8
Yevhenii Nabok <1337niceshot@gmail.com>
2024-03-12 20:32:20 +0200

    init

---
+++ src/ChatApp.Server.Api/HostingExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ChatApp.Server.Api;
+
+public static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+
+        return app;
+    }
+}
