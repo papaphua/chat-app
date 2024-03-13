9d8f3d2cd64df2b4cbb0b3063810d1bd4fcc5aeb
Yevhenii Nabok <1337niceshot@gmail.com>
2024-03-13 17:12:14 +0200

    add repo register

--- src/ChatApp.Server.Api/HostingExtensions.cs
+++ src/ChatApp.Server.Api/HostingExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using ChatApp.Server.Application.Core.Abstractions;
+using ChatApp.Server.Domain.Core.Abstractions;
 using ChatApp.Server.Domain.Users;
 using ChatApp.Server.Infrastructure;
+using ChatApp.Server.Infrastructure.Core.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 namespace ChatApp.Server.Api;
@@ -11,17 +13,28 @@ public static class HostingExtensions
     {
         var defaultConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
+        builder.Services.AddControllers();
+
         builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(defaultConnection));
 
         builder.Services.AddScoped<IUnitOfWork>(provider => provider.GetRequiredService<ApplicationDbContext>());
 
         builder.Services.AddIdentityCore<User>().AddEntityFrameworkStores<ApplicationDbContext>();
 
+        builder.Services.Scan(scan => scan
+            .FromAssembliesOf(typeof(Repository<>), typeof(IRepository<>))
+            .AddClasses(classes => classes
+                .AssignableToAny(typeof(Repository<>)))
+            .AsMatchingInterface()
+            .WithScopedLifetime());
+
         return builder.Build();
     }
 
     public static WebApplication ConfigurePipeline(this WebApplication app)
     {
+        app.MapControllers();
+
         return app;
     }
 }
