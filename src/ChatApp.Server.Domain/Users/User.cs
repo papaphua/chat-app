4454eb1f95264b9b1b9c3b37dcb0a4276724a68a
Yevhenii Nabok <1337niceshot@gmail.com>
2024-03-13 14:15:52 +0200

    update user entity

--- src/ChatApp.Server.Domain/Users/User.cs
+++ src/ChatApp.Server.Domain/Users/User.cs
@@ -1,4 +1,7 @@
-﻿using ChatApp.Server.Domain.Core.Abstractions;
+﻿using ChatApp.Server.Domain.Contacts;
+using ChatApp.Server.Domain.Core.Abstractions;
+using ChatApp.Server.Domain.Directs;
+using ChatApp.Server.Domain.Groups;
 using Microsoft.AspNetCore.Identity;
 
 namespace ChatApp.Server.Domain.Users;
@@ -10,4 +13,12 @@ public sealed class User : IdentityUser<Guid>, IEntity<Guid>
     public string? LastName { get; set; }
 
     public string? Bio { get; set; }
+
+    public ICollection<UserAvatar> Avatars { get; set; } = default!;
+
+    public ICollection<Contact> Contacts { get; set; } = default!;
+
+    public ICollection<DirectMembership> DirectMemberships { get; set; } = default!;
+    
+    public ICollection<GroupMembership> GroupMemberships { get; set; } = default!;
 }
