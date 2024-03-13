45fea984774e19d1412c3a7f0b86f66eaf9c875a
Yevhenii Nabok <1337niceshot@gmail.com>
2024-03-13 17:59:10 +0200

    add api controller abstr

---
+++ src/ChatApp.Server.Api/Core/Abstractions/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Server.Api.Core.Abstractions;
+
+[ApiController]
+public abstract class ApiController : ControllerBase
+{
+    public Guid UserId =>
+        // var stringId = HttpContext.User.FindFirstValue("sub");
+        // return Guid.TryParse(stringId, out var guid)
+        //     ? guid
+        //     : Guid.Empty;
+        Guid.Parse("4CD62940-6744-4BAD-83BB-B2B1830A2BB7");
+}
