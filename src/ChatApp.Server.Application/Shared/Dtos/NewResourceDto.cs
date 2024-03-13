c22c4bab0dc49c08f3c8c7117a38a3f4a843a853
Yevhenii Nabok <1337niceshot@gmail.com>
2024-03-13 21:57:46 +0200

    add form file ext

---
+++ src/ChatApp.Server.Application/Shared/Dtos/NewResourceDto.cs
@@ -0,0 +1,12 @@
+﻿using ChatApp.Server.Domain.Resources;
+
+namespace ChatApp.Server.Application.Shared.Dtos;
+
+public sealed class NewResourceDto
+{
+    public string Name { get; set; } = default!;
+
+    public byte[] Bytes { get; set; } = default!;
+
+    public FileExtension Extension { get; set; } = default!;
+}
---
+++ src/ChatApp.Server.Domain/Core/AvatarExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ChatApp.Server.Domain.Resources;
+
+namespace ChatApp.Server.Domain.Core;
+
+public static class AvatarExtensions
+{
+    private static readonly Dictionary<string, FileExtension> AvatarExtensionMapping = new()
+    {
+        { "jpg", FileExtension.Jpg },
+        { "jpeg", FileExtension.Jpeg },
+        { "png", FileExtension.Png },
+        { "gif", FileExtension.Gif }
+    };
+}
