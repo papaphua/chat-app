a52d310127c267a55c7e4f725d6f0dc87d89d189
Yevhenii Nabok <1337niceshot@gmail.com>
2024-03-30 00:08:48 +0200

    update spacing

---
+++ src/ChatApp.Client/Pages/Profile.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace ChatApp.Client.Pages;
+
+[Authorize]
+public sealed partial class Profile
+{
+    private IBrowserFile? File { get; set; }
+}
