6dfe6ac754030a11085a5b52d93f257175fb69c0
Yevhenii Nabok <1337niceshot@gmail.com>
2024-03-31 17:52:11 +0300

    add dialogs

---
+++ src/ChatApp.Client/Pages/Profile/EmailConfirmationDialog.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace ChatApp.Client.Pages.Profile;
+
+public sealed partial class EmailConfirmationDialog
+{
+    [CascadingParameter] private MudDialogInstance MudDialog { get; set; } = default!;
+
+    private void Submit() => MudDialog.Close(DialogResult.Ok(true));
+    
+    private void Cancel() => MudDialog.Cancel();
+}
